336600eeb0632b24bb5240344189c805fcaf85ed
Giorgi Dzebisashvili <giorgidzebisashvili4@gmail.com>
2023-10-16 18:25:28 +0400

    finished

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
 let color = "black"
+let click = false
 // boots up when html&css is loaded
 document.addEventListener("DOMContentLoaded",function(){
     createBoard(16)
+    document.querySelector("body").addEventListener("click",function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.textContent = "Now You Can Draw"
+            }else{
+                draw.textContent = "You're Not Allowed To Draw"
+            }
+        }
+    })
+    
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener('click',()=>{
        let size = getSize();
@@ -38,12 +51,14 @@ function getSize(){
 }
 
 function colorDiv(){
+    if(click){
     if (color == "random"){
         this.style.backgroundColor =  `hsl(${Math.random()*360},100%,50%)`;
     }
     else{
         this.style.backgroundColor = 'black'
     }
+}
 }
 
 function setColor(colorChoice){

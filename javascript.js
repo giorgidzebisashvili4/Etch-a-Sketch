34379de1ba883a4e4c627c8f1c2d80f60db8df6a
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2023-11-21 16:37:57 +0400

    failed on input conection to js, moving on background color change

--- javascript.js
+++ javascript.js
@@ -1,15 +1,19 @@
 let container= document.querySelector("body")
+let input = document.querySelector('input')
+let inputBtn = document.querySelector('button')
 
 let gridSize = 5
+grid()
+
+function grid(){
 
 let gridMaker = document.createElement('div')
 gridMaker.classList.add("container")
 gridMaker.style.cssText = `display: grid; grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`
 
-
-let n = gridSize**2
+let DivsCount = gridSize**2
  
-for(let i=0; i<n; i++){
+for(let i=0; i<DivsCount; i++){
 let divsMaker = document.createElement('div')
 divsMaker.classList.add("test")
 
@@ -17,3 +21,11 @@ gridMaker.appendChild(divsMaker)
 }
 
 container.appendChild(gridMaker)
+}
+
+inputBtn.addEventListener('click',()=>{
+    gridSize=input.value
+    console.log(gridSize)
+    // container.removeChild(gridMaker)
+    grid()
+})

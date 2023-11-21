d7ec99cc265a08dddf0fd5866941f95e47043821
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2023-11-21 17:58:10 +0400

    background color reset after input

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ let input = document.querySelector('input')
 let inputBtn = document.querySelector('button')
 
 
-let gridSize = 16
-grid()
-
-function grid(){
+let gridSize = 3
 
 let gridMaker = document.createElement('div')
 gridMaker.classList.add("container")
+grid()
+function grid(){
+
 gridMaker.style.cssText = `display: grid; grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`
 
 let DivsCount = gridSize**2
@@ -22,23 +22,27 @@ gridMaker.appendChild(divsMaker)
 }
 
 container.appendChild(gridMaker)
-}
-
-inputBtn.addEventListener('click',()=>{
-    gridSize=input.value
-    console.log(gridSize)
-    // container.removeChild(gridMaker)
-    grid()
-})
+// change color to white after ne numbers
 
 let divSelect = document.querySelectorAll(".container>div")
 
 divSelect.forEach((divSelect)=>{
+    divSelect.style.background="white"
 divSelect.addEventListener('mouseover',(e)=>{
-    e.target.style.background="blue"
+    e.target.style.background="grey"
     console.log(e.target)
     })
 })
+
+}
+
+inputBtn.addEventListener('click',()=>{
+    if(input.value<=100){
+        gridSize=input.value
+    }else{alert("max is 100!")}
+    grid()
+})
+
 
 
 
